c3246fcf058f06e981b88ea144ae2effa4a327bd
abilioj <ajgfqbu@gmail.com>
2022-11-09 01:08:13 -0300

    Update iniciando a tela de cadastrar

--- project/pim_diagn_covid19/main.c
+++ project/pim_diagn_covid19/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-#define SIZE 200
+#define SIZE 200 //tamanho do camos de dados text
 
 /* ---------------------- dados Paciente -----------------------------
     - Nome; CPF; Telefone; Endereço (rua, número, bairro, cidade, estado e CEP)
@@ -11,21 +11,29 @@
     - Comorbidade do paciente (diabetes, obesidade, hipertensão,tuberculose, outros)
 */
 
-//Estrutuda de dados de paciente
+//- Estrutuda de dados de paciente
 struct dados_paciente
 {
     char nome[SIZE];
     char email[SIZE];
-    int numero, dia, mes, ano;
-    char cpf[SIZE], telefone[SIZE], cep[SIZE], diag[SIZE];
+    int numero, diaNas, mesNas, anoNas;
+    char cpf[11], telefone[15], cep[10], diaDiag[11];
     char rua[SIZE], bairro[SIZE], cidade[SIZE], estado[SIZE], comorbidade[10], quais[SIZE];
 };
-struct dados_paciente pac;
+struct dados_paciente paciente;
+
+// Variaveis Globais
+// - ponteiro para struct que armazena data e hora
+struct tm *data_hora_atual;
+
+// - variável do tipo time_t para armazenar o tempo em segundos
+time_t segundos;
 
 //declaração dos metodos
 void menu();
 void cadastrar_paciente();
 void listar_pacientes();
+void listar_pacientes_comorbidade();
 
 int main()
 {
@@ -35,14 +43,13 @@ int main()
 }
 
 void menu(){
-   int op;
-
+    int op;
     do{
-        //system("cls");
-        printf("\n1 - Cadastrar Paciente \n 2 - Listar Paciente");
-        printf("\n0 - Sair\n");
+        system("cls");
+        printf(" -------------------- Menu de Opcoes -------------------- ");
+        printf("\n 1 - Cadastrar Paciente \n 2 - Listar todos os Pacientes \n 3 - Listar Pacientes com Comorbidade");
+        printf("\n 0 - Sair\n");
         scanf("%d", &op);
-
         switch(op){
         case 0:
             printf("Fechando! Ate logo...\n\n");
@@ -54,13 +61,35 @@ void menu(){
             break;
         case 3:
             break;
-        case 4:
-            break;
         default:
             printf("Opcao invalida.\n\n");
         }
     }while(op != 0);
 }
 
-void cadastrar_paciente(){
+void cadastrar_paciente()
+{
+    int op, dataAtual[3];
+    //obtendo o tempo em segundos atual da maquina
+    time(&segundos);
+    //para converter de segundos para o tempo local
+    //utilizamos a função localtime
+    data_hora_atual = localtime(&segundos);
+    dataAtual[0] = data_hora_atual->tm_mday;
+    dataAtual[1] = data_hora_atual->tm_mon+1;
+    dataAtual[2] = data_hora_atual->tm_year+1900;
+
+    do{
+        system("cls");
+        printf(" -------------------- Cadastrar Paciente -------------------- ");
+        printf("\n\tData Atual: %d/%d/%d \n", dataAtual[0], dataAtual[1], dataAtual[2]);
+
+        printf("\n\tNome:");
+        printf("\n\tCPF:");
+        printf("\n\tTelefone:");
+
+        printf("\n\n\n\n Digite: 1) - Para Cadastrar novamente um Paciente ou 0) - Sair dessa opcao\n");
+        scanf("%d", &op);
+
+    } while (op != 0);
 }
