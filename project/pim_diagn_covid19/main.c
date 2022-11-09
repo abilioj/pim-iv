0773b36383e0e9e219c00cd7bd99e00145a6c0b3
abilioj <ajgfqbu@gmail.com>
2022-11-08 21:11:02 -0300

    Update ADD

---
+++ project/pim_diagn_covid19/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* ---------------------- dados Paciente -----------------------------
+    - Nome.
+    - CPF.
+    - Telefone.
+    - Endereço (rua, número, bairro, cidade, estado e CEP)
+    - Data de nascimento.
+    - E mail.
+    - Data do diagnóstico.
+    - Comorbidade do paciente (diabetes, obesidade, hipertensão,tuberculose, outros
+*/
+
+
+int main()
+{
+    printf("Olá Mundo!\n");
+    return 0;
+}
